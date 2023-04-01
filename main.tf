3f6abe824aa44fb3f24618a7b1663afebcb295ea
Joann Ren <joann.ren@xylem.com>
2023-03-31 18:23:05 -0600

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    orgization = "ACG-Terraform-Demos-JR"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-github-actions"
     }
   }
 }
